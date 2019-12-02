87c01cd554f889154985504f45e7db54997775af
funny <cooljele@gmail.com>
2019-12-02 22:11:49 +0800

    Initial Commit

---
+++ TYSMAlgorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TYSMAlgorithm
+//
+//  Created by jele lam on 2/12/2019.
+//  Copyright © 2019 CookiesJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
